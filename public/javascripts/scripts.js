e68ccd6fdde612c6ec4b4e45d0cc7923cd5f40ea
incometrader <igdefinition@gmail.com>
2017-02-24 06:57:54 +0100

    fix bugs in frontend javascript

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -9,98 +9,115 @@ window.onload = function windowLoad() {
   };
 
 	firebase.initializeApp(config);
-	editorLink = localStorage.getItem("userAddress");
-	// if (editorLink){
-	// 	alert(editorLink);
-	// }
+	var editorLink = localStorage.getItem("userAddress");
   var signInRedirect = editorLink ? editorLink : '/pair';
-	// FirebaseUI config.
 	var uiConfig = {
-		// Query parameter name for mode.
 		'queryParameterForWidgetMode': 'mode',
-		// Query parameter name for sign in success url.
 		'queryParameterForSignInSuccessUrl': 'signInSuccessUrl',
-		// Will use popup for IDP Providers sign-in flow instead of the default, redirect.
 		'signInFlow': 'popup',
 		'signInSuccessUrl': signInRedirect,
 		'signInOptions': [
-			// Leave the lines as is for the providers you want to offer your users.
 			firebase.auth.GoogleAuthProvider.PROVIDER_ID,
 			firebase.auth.FacebookAuthProvider.PROVIDER_ID,
 			firebase.auth.EmailAuthProvider.PROVIDER_ID
 		],
-		// Terms of service url.
 		'tosUrl': '/',
 		'callbacks': {
 			'signInSuccess': function (currentUser, credential, redirectUrl) {
-				window.localStorage.user = currentUser;
-				// Do something.
-				// Return type determines whether we continue the redirect automatically
-				// or whether we leave that to developer to handle.
+				localStorage.setItem("user", currentUser);
 				return true;
 			}
 		}
 	};
 
-	if (window.location.pathname === '/login' && window.localStorage.user) {
+  if (localStorage.getItem("user") !== "undefined") {  
+		if (window.location.pathname === '/pair') {
+			function firePadInit() {
+				var firepadRef = getExampleRef(); 
+				var editor = ace.edit("firepad-container");
+				editor.setTheme("ace/theme/monokai");
+				editor.$blockScrolling = Infinity;
+				var session = editor.getSession();
+				session.setUseWrapMode(true);
+				session.setUseWorker(false);
+				session.setMode("ace/mode/javascript");
+				var firepad = Firepad.fromACE(firepadRef, editor, {
+					defaultText: '// You are now coding in Javascript!\nfunction TIA() {\n  var message = "This is Andela.";\n  console.log(message);\n}'
+				});
+				document.getElementById('pairURL').href = link;
+				document.getElementById('pairURL').innerHTML = link;
+			}
+
+			function getExampleRef() {
+				var ref = firebase.database().ref();
+				var hash = window.location.hash.replace(/#/g, '');
+				if (hash) {
+					ref = ref.child(hash);
+					link = window.location;
+				} else {
+					ref = ref.push();
+					window.location = '/pair' + '#' + ref.key;
+					link = window.location;
+				}
+
+				if (typeof console !== 'undefined') {
+					console.log('Firebase data: ', ref.toString());
+				}
+				return ref;
+			}
+
+			function initChat(user) {
+				// Get a Firebase Database ref
+				var chatRef = firebase.database().ref("chat");
+
+				// Create a Firechat instance
+				var chat = new FirechatUI(chatRef, document.getElementById("firechat-wrapper"));
+
+				// Set the Firechat user
+				chat.setUser(user.uid, user.displayName);
+			}
+
+				// hide chat and code
+			// document.getElementById("pair1").style.display = 'none'; 
+			// document.getElementById("chat-btn").addEventListener("click", switchToChat);
+			// document.getElementById("code-btn").addEventListener("click", switchToCode);
+			// function switchToChat(pair1, pair2) {
+    	// 	return document.getElementById(pair1).style.display = 'none';
+			// }
+			// function switchToCode(pair1, pair2) {
+    	// 	return document.getElementById(pair2).style.display = 'none';
+			// }
+
+			
+
+			// chat
+			firebase.auth().onAuthStateChanged(function(user) {
+				if (user) {
+					initChat(user);
+				}
+			});
+		}
+	} else {
+		// no user
+		document.getElementById("signout-nav").style.display = 'none';
+		document.getElementById("pair-nav").style.display = 'none';  
+		if (window.location.pathname === '/pair') {
+			window.location = '/login';
+		}
+	}
+
+	if (window.location.pathname === '/login') {
 		var ui = new firebaseui.auth.AuthUI(firebase.auth());
-		// The start method will wait until the DOM is loaded.
 		ui.start('#firebaseui-auth-container', uiConfig);
 	}
-	// // do stuff based on login staus
-  // if( window.localStorage.user) {
 
-	// } else {
+  // if(localStorage.getItem("user")) {
+	// 	document.getElementById("login-nav").style.display = 'none';
+	// 	document.getElementById("signup-nav").style.display = 'none';
+	// } 
 
-	// }
-	// check if user is signed in
-	firebase.auth().onAuthStateChanged(function (user) {
-		window.localStorage.user = user;
-	});
-
-	function firePadInit() {
-		// Get Firebase Database reference.
-		var firepadRef = getExampleRef(); 
-		
-		//// Create ACE
-		var editor = ace.edit("firepad-container");
-		editor.setTheme("ace/theme/monokai");
-		editor.$blockScrolling = Infinity;
-		var session = editor.getSession();
-		session.setUseWrapMode(true);
-		session.setUseWorker(false);
-		session.setMode("ace/mode/javascript");
-		// Create Firepad.
-		var firepad = Firepad.fromACE(firepadRef, editor, {
-			defaultText: '// JavaScript Editing with Firepad!\nfunction go() {\n  var message = "Hello, world.";\n  console.log(message);\n}'
-		});
-		document.getElementById('pairURL').href = link;
-    document.getElementById('pairURL').innerHTML = link;
-	}
-
-
-	// Helper to get hash from end of URL or generate a random one.
-	function getExampleRef() {
-		var ref = firebase.database().ref();
-		var hash = window.location.hash.replace(/#/g, '');
-		if (hash) {
-			ref = ref.child(hash);
-			link = window.location;
-		} else {
-			ref = ref.push(); // generate unique location.
-			link = window.location; // add it as a hash to the URL.
-
-			window.location = '/pair' + '#' + ref.key;
-		}
-
-		if (typeof console !== 'undefined') {
-			console.log('Firebase data: ', ref.toString());
-		}
-		return ref;
-	}
-
-	if (window.location.pathname.indexOf('/pair') !== -1) {
-		if (window.localStorage.user){
+	if (window.location.pathname === '/pair') {
+		if (localStorage.getItem("user")){
 			firePadInit();
 		} else {
 			window.location.href = "/login";
@@ -119,13 +136,5 @@ window.onload = function windowLoad() {
 			console.error('Sign Out Error', error);
 		});
 	}
-	/*function notLoggedIn(){
-		var notAUser = document.getElementById("signout-home");
-		notAUser.innerHTML = <div class="alert alert-warning alert-dismissible" role="alert">
-  	<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-  	You are not logged ln. Please log in to continue;
-		</div>;
-		console.log("You are not a user");
-	}
-	*/
+	
 };
